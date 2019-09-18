4c113e46b6825b86a53c5f7d6f8ea0f972bfdc85
Stepan Oskin <stepanoskin007@gmail.com>
2019-09-18 15:41:08 -0400

    add SBS class (Sequential Backwards Selection algorithm)

--- src/model_utils.py
+++ src/model_utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from itertools import combinations
+from sklearn import clone
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, Perceptron
 from sklearn.metrics import accuracy_score
@@ -12,6 +14,60 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from scipy.stats import norm, shapiro, normaltest, anderson
 from time import time
+
+
+class SBS():
+    def __init__(self, estimator, k_features,
+                 scoring=accuracy_score,
+                 test_size=0.3, random_state=1):
+        self.scoring = scoring
+        self.estimator = clone(estimator)
+        self.k_features = k_features
+        self.test_size = test_size
+        self.random_state = random_state
+
+    def fit(self, X, y):
+
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=self.test_size,
+                             random_state=self.random_state)
+
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(X_train, y_train,
+                                 X_test, y_test, self.indices_)
+        self.scores_ = [score]
+
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+
+            for p in combinations(self.indices_, r=dim - 1):
+                score = self._calc_score(X_train, y_train,
+                                         X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+
+            self.scores_.append(scores[best])
+        self.k_score_ = self.scores_[-1]
+
+        return self
+
+    def transform(self, X):
+        return X[:, self.indices_]
+
+    def _calc_score(self, X_train, y_train, X_test, y_test,
+                    indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
 
 
 def plot_decision_regions(X, y, classifier, test_idx=None,
