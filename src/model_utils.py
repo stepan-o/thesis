619f9f3faa6cfd3b5f37ab9bce27a91f75e665d9
Stepan Oskin <stepanoskin007@gmail.com>
2019-10-06 18:26:16 -0400

    add plot_confusion_matrix

--- src/model_utils.py
+++ src/model_utils.py
@@ -13,9 +13,11 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.utils.multiclass import unique_labels
 from scipy.stats import norm, shapiro, normaltest, anderson
 from time import time
+
 
 
 class SBS():
@@ -402,6 +404,66 @@ def plot_validation_curve(classifier, model_name, X, y, param_name, param_range,
     print("Validation curve for {0} plotted, took {1:,.2f} seconds ({2:,.2f} minutes)"
           .format(model_name, elapsed, elapsed / 60))
     plt.show()
+
+
+def plot_confusion_matrix(y_true, y_pred, classes, model_name,
+                          normalize=False,
+                          interpolation='nearest',
+                          cmap=plt.cm.Blues,
+                          width=4):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        title = '{0}\nNormalized confusion matrix'.format(model_name)
+    else:
+        title = '{0}\nConfusion matrix, without normalization'.format(model_name)
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("{0}\nNormalized confusion matrix".format(model_name))
+    else:
+        print('{0}\nConfusion matrix, without normalization'.format(model_name))
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    plt.grid(False)
+    fig.set_size_inches((width, width))
+
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes)
+    ax.tick_params(labelsize=16)
+    ax.set_xlabel('Predicted label', fontsize=18)
+    ax.set_ylabel('True label', fontsize=18)
+    ax.set_title(title, fontsize=18)
+
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=0, ha="center",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else ',d'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black",
+                    fontsize=16)
+    fig.tight_layout()
+    return ax
 
 
 def fit_norm_dist(series, h_bins='auto',
